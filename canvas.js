25d698a5191de9cd75e00ea8a1be9b1f3b3ace3c
Noah Fichter <nfichter@stuy.edu>
2017-02-14 14:07:09 -0500

    woooo

--- canvas.js
+++ canvas.js
@@ -15,7 +15,41 @@ var clearCanvas = function() {
     ctx.clearRect(0,0,c.width,c.height);
 };
 
+lastX = -1;
+lastY = -1;
+
+var drawPath = function(e) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    if (lastX == -1 && lastY == -1) {
+	lastX = mouseX;
+	lastY = mouseY;
+    } else {
+	ctx.strokeStyle = "rgb("+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+")";
+	ctx.beginPath();
+	ctx.moveTo(lastX,lastY);
+	ctx.lineTo(mouseX,mouseY);
+	ctx.stroke();
+	ctx.fillStyle = "rgb("+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+")";
+	ctx.beginPath();
+	ctx.moveTo(lastX,lastY);
+	ctx.arc(lastX,lastY,10,0,Math.PI*2);
+	ctx.stroke();
+	ctx.fill();
+	lastX = mouseX;
+	lastY = mouseY;
+    };
+};
+
 var clr = document.getElementById("clr");
 clr.addEventListener("click",clearCanvas);
 
-c.addEventListener("click",drawSquare);
+c.addEventListener("click",drawPath);
+//c.addEventListener("click",drawSquare);
+
